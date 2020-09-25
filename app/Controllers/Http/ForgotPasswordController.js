14b059254cce2a621e9e12e61e946490d04003f2
Fagner Egidio Gomes <fagner.gomes@compasso.com.br>
2020-09-25 17:12:20 -0300

    feat: 🎸 Implementação do metodo para recuperação de senha

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -2,6 +2,7 @@
 
 const crypto = require('crypto')
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store({ request, response }) {
@@ -13,6 +14,21 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        (message) => {
+          message
+            .to(user.email)
+            .from('fagner.egomes@gmail.com', 'Fagner Gomes')
+            .subject('Recuperação de senha')
+        }
+      )
     } catch (error) {
       return response.status(error.status).send({
         error: { message: 'Algo não deu certo, este e-mail existe? ' }
